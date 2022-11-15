3209453d35a629f4cf186f70c72c3d7152c86241
Benjamin <benny.klotz92@gmail.com>
2022-11-15 15:26:14 +0100

    Explicitly connect on application startup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { ClientKafka, EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
   private count = 0;
   private count2 = 0;
 
   constructor(@Inject('KAFKA_CLIENT') private client: ClientKafka) {}
+
+  async onModuleInit() {
+    await this.client.connect();
+  }
 
   @Get('kafka')
   async publisherKafka() {
